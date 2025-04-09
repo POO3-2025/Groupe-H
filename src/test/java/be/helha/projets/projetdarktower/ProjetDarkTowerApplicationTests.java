ba35d63de99475e2686f489e4e8dd9b3d1089413
LA246517-SBAI <la246517@student.helha.be>
2025-04-09 18:13:24 +0200

    Test 1

---
+++ src/test/java/be/helha/projets/projetdarktower/ProjetDarkTowerApplicationTests.java
@@ -0,0 +1,13 @@
+package be.helha.projets.projetdarktower;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetDarkTowerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
