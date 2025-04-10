ecdf3e6d478f03164541b9881523a3d73ddd601c
LA246517-SBAI <la246517@student.helha.be>
2025-04-10 16:40:13 +0200

    Creation du main dans le package Inscription

---
+++ src/main/java/be/helha/projets/projetdarktower/Inscription/Main.java
@@ -0,0 +1,7 @@
+package be.helha.projets.projetdarktower.Inscription;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Bienvenue dans l'application Projet Dark Tower !");
+    }
+}
