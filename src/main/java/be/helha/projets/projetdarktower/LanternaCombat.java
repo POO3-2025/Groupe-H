ec5f925a62c489b997a38e066e17c5ab20546903
LA246517-SBAI <la246517@student.helha.be>
2025-05-13 16:14:50 +0200

    Modif

--- src/main/java/be/helha/projets/projetdarktower/LanternaCombat.java
+++ src/main/java/be/helha/projets/projetdarktower/LanternaCombat.java
@@ -61,15 +61,14 @@ public class LanternaCombat {
             e.printStackTrace();
         }
     }
-    private static void showMainMenu(WindowBasedTextGUI gui, Screen screen) {
+    private static void showMainMenu(MultiWindowTextGUI gui, Screen screen) {
         BasicWindow window = new BasicWindow("Menu DarkTower");
         Panel panel = new Panel(new GridLayout(1));
         String introText = "\nDarkTower, le jeu où franchir les étages d'une tour semble impossible. "
                 + "\nChaque défi devient plus complexe, rendant l'ascension inaccessibile. "
                 + "\nLa tour est un piège où l'échec est inévitable.";
         panel.addComponent(new Label(introText));
-        panel.addComponent(new Label("\n===== MENU DarkTower ====="));
-        panel.addComponent(new EmptySpace());
+        panel.addComponent(new Label("===== MENU DarkTower ====="));
         panel.addComponent(new Button("1. S'inscrire", () -> {
             window.close();
             showRegisterMenu(gui);
